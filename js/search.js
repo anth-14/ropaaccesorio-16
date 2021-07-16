c09ec0ec794357ab060116a7cedcd8f906bb9dc3
anth-14 <mh16022@ues.edu.sv>
2021-07-16 16:53:00 -0600

    first commit

---
+++ js/search.js
@@ -0,0 +1,12 @@
+var buscador = $("#table").DataTable();
+
+$("#input-search").keyup(function(){
+    
+    buscador.search($(this).val()).draw();
+    
+    if ($("#input-search").val() == ""){
+        $(".content-search").fadeOut(100);
+    }else{
+        $(".content-search").fadeIn(100);
+    }
+})
